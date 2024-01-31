26edbe002177f329a3574876eaf6ae8b54396927
MICHELSVETOVBASED <147500401+MICHELSVETOVBASED@users.noreply.github.com>
2024-01-31 17:29:05 +0100

    d

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+
+public class Solution{
+    public bool 
+    public int RomanToInt(string s){
+        int count = 0;
+        char[] ch = s.ToCharArray();
+        int valVal, nextVal;
+        for (int i = 0; i < ch.Length; i++){
+            valVal = map[ch[i]];
+            
+            if (i != ch.Length-1){
+                nextVal = map[ch[i + 1]];
+                if (nextVal > valVal){
+                    count += nextVal - valVal;
+                    i++;
+                    if ( ch[i+1].)
+                }
+            }
+
+            count += valVal;
+
+        }
+
+        return count;
+    }
+
+    private readonly Dictionary<char, int> map = new Dictionary<char, int>
+        { { 'I', 1 },{ 'V', 5 },{ 'X', 10 },{ 'L', 50 },{ 'C', 100 },{ 'D', 500 },{ 'M', 1000 } };
+
+    static void Main(){
+        var a = new Solution();
+        int ss = a.RomanToInt("MCMXCIV");
+        Console.WriteLine(ss);
+    }d
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Voluptously_sloopness")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Voluptously_sloopness")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("773EC311-EAC6-4DD9-9176-1FB672F78F69")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
